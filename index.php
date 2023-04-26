b8b81c00203387eebb23f0d84943e6f341590d21
Anthony Stanyer <121306984+stanyer75@users.noreply.github.com>
2023-04-26 13:04:59 +0100

    Array filtering

--- index.php
+++ index.php
@@ -1,14 +1,56 @@
 <?php
 
+class post
+{
+    public $title;
+    public $published;
+
+    public function __construct($title, $published)
+    {
+        $this->title = $title;
+        $this->published = $published;
+
+    }
+
+}
+
+$posts = [
+
+    new post ('My First Post', true),
+    new post ('My second Post', true),
+    new post ('My third Post', true),
+    new post ('My fourth Post', false)
+
+];
+
+/* $unpublishedPosts = array_filter($posts, function($posts){
+
+    return ! $posts->published;
+
+});
+
+$publishedPosts = array_filter($posts, function($posts){
+
+    return  $posts->published;
+
+});
+
+var_dump($publishedPosts); */
 
 
-$database = require 'bootstrap.php';
+/* $modified = array_map(function ($post){
 
-$tasks = $database -> selectAll('todos');
+    $post->published = true;
+    return $post;
 
+}, $posts);
 
+var_dump($modified); */
 
-require 'index.view.php';
+$posts = array_map(function($post){
+        return (array) $post;
+}, $posts);
 
+$titles = array_column($posts, 'title', 'published');
 
- 
+var_dump($titles);
