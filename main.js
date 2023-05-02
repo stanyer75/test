6a6cf82b01a433e78813542e12e5343136bb0e5e
Anthony Stanyer <121306984+stanyer75@users.noreply.github.com>
2023-05-02 11:20:07 +0100

    Sets and maps

--- main.js
+++ main.js
@@ -1,41 +1,67 @@
 'use strict';
+/*
+const fruit = new Map();
 
-async function init(){
-    const start = Date.now();
-    document.getElementById('output').innerHTML = `0: init()`;
+fruit.set(1, 'banna');
+fruit.set(2, 'apple');
+fruit.set(3, 'strawberry');
+*/
+/*
+document.getElementById('output').innerHTML = fruit.get(2);
+
+fruit.delete(2);
 
 
-    const userPromise = getUserData();
-    const welcomeStringPromise = getWelcomeString();
+fruit.forEach(item => { 
+    document.getElementById('output2').innerHTML  += '<br>' + item;    
+}); */
+/*
+fruit.set (4, {
+    type: 'Orange',
+    colour: 'orange',
+    flavour: ' orangey'
+});
 
-    const user = await userPromise;
-    document.getElementById('output2').innerHTML = `${Date.now() - start}: ${user.name}`;
+document.getElementById('output').innerHTML = fruit.get(4).flavour; */
 
-    const welcomeString = await welcomeStringPromise;
-    document.getElementById('output3').innerHTML = `${Date.now() - start}: ${welcomeString}`;
 
+
+
+
+const users = new Map(); // maps are on a value to key basis, much like a dictionary
+
+users.set({
+    name: 'Andrew',
+    email: 'email'
+
+}, function(user){
+    document.getElementById('output').innerHTML += `${user.name} (${user.email}) <br>`;
+});
+
+const userAnthony = {
+    name: 'Anthony',
+    email: 'email 2'
 }
-    
+
+users.set(userAnthony, function(user){
+    document.getElementById('output2').innerHTML += `${user.email} (${user.name}) <br>`;
+});
+
+users.forEach((value, key) => {
+    value(key)
+});
+
+let userAnthonyExists = users.has(userAnthony);
+
+console.log('userAnthonyExists', userAnthonyExists);
 
 
-function getUserData(){
-    return new Promise((resolve, reject) => {
-        let user = {
-            name: 'Andrew',
-            email: 'email here'
-        };
-        setTimeout (() => {
-            resolve(user);
-        },2000);
-    });
-}
+const fruit = new Set(); // sets only contain values, no keys like maps do 
 
-function getWelcomeString(){
-    return new Promise((resolve,reject) => {
-        setTimeout(() => {
-            resolve('Welcome to my asyncronous program!');
-        },2000);
-    })
-};
+fruit.add('banna') // cannot have duplicate value however
+fruit.add('Apple')
+fruit.add('Strawberry')
 
-init();
+fruit.forEach(value => {
+    document.getElementById('output3').innerHTML += '<br>' + value
+});
