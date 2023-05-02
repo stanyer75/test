707f1d42d81d9df2311c83f5881d4af5f2a93478
Anthony Stanyer <121306984+stanyer75@users.noreply.github.com>
2023-05-02 10:05:22 +0100

    Promises

--- main.js
+++ main.js
@@ -1,45 +1,41 @@
 'use strict';
 
-class Item {
-    constructor(name, category){
-        this.name = name;
-        this.category = category;
-    }
+let myPromise = new Promise((resolve,reject) =>{
 
-    static maxItems = 10;
+    let user = {
 
-    static getHelperText(){
-        return 'add items to your list'
-    }
+        name: 'andrew',
+        email: 'andrew@exaple.com'
+    };
 
-    getDetails(){
-        return `${this.name} - ${this.category}`;
-    }
+    setTimeout(()=>{
+        resolve(user); //if user is retrieveable then this code will execute
+        //reject('could not retrieve user') //if not retrievable then this will display within the console of the website not neccessarily on the page
+    }, 2000);       /* a delay of two seconds is set to emulate pulling in or accessing large files*/
+
+});
+
+const getAdditionalUserData = user => {
+
+    document.getElementById('output'). innerHTML = `${user.name} (${user.email})`; 
+
+    return new Promise((resolve, reject)=> {
+       
+        user.favouriteColor = 'Blue',
+        user.currentDrink = 'La croix'
+
+        setTimeout(()=>{
+            resolve(user);
+        }, 2000);
+    });
 }
 
-class PurchasedItem extends Item {
-    constructor(name, category, price){
-        super(name, category);
-        this.price = price;
+myPromise.then(getAdditionalUserData)
+.then((user) => {
+    document.getElementById('output'). innerHTML = `${user.name} (${user.email}) ${user.currentDrink} ${user.favouriteColor}`;
+})
+.catch((error) => {   //catch is just tagged onto the end of the promise executor 
+    document.getElementById('output').innerHTML = error; // this code displays the error when a rejection occurs and cancels all code thereafter
+});
 
-    }
-
-    getDetailsWithPrice(){
-        return `${this.name} - ${this.category} - $${this.price}`;
-    }
-    
-    static getNumberOfItems(){
-        return `3/ ${super.maxItems}`;
-    }
-}
-
-/* let item = new Item('coffee', 'food');
-item.category = 'drinks';
-
-let purchasedItem = new PurchasedItem('sugar', 'food', '2.49');  */
-
-/* document.getElementById('output').innerHTML = Item.getHelperText(); */               
-
-/*either or these, not both, output is variable set back in index and can be only called and set once*/ 
-
-document.getElementById('output').innerHTML = PurchasedItem.getNumberOfItems();
+document.getElementById('output').innerHTML = 'text here!!' /* this code will be true and execute for two seconds in lue of the promise being resolved up top*/ 
